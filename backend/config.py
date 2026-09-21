607c7ac2b4176d653a73a13d4820ed7e08876439
Maroc-CTF <maroc-ctf@outlook.com>
2026-09-21 12:47:42 +0100

    the great API call fix

--- backend/config.py
+++ backend/config.py
@@ -4,4 +4,4 @@ API_URL = os.getenv("API_URL", "https://api.atlas-heritage.ma")
 MAPS_API_KEY = os.getenv("MAPS_API_KEY")
 
 if not MAPS_API_KEY:
-    MAPS_API_KEY = ""
+    MAPS_API_KEY = "ADV{git_ma_kaynsa_chi_haja}"
